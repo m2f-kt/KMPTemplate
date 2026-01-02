6c75b13e25f0479be94b360b3d72ce2983121477
Marc Moreno Ferrer <ignis.fatue@gmail.com>
2026-01-02 12:02:53 +0100

    feat(server): upadte database migartion table

--- server/core/database/src/main/kotlin/com/m2f/core/database/migrations/MigrationsTable.kt
+++ server/core/database/src/main/kotlin/com/m2f/core/database/migrations/MigrationsTable.kt
@@ -1,6 +1,8 @@
 package com.m2f.core.database.migrations
 
 import org.jetbrains.exposed.v1.core.Table
+import org.jetbrains.exposed.v1.datetime.datetime
+import org.jetbrains.exposed.v1.datetime.CurrentDateTime
 
 private const val VERSION_VARCHAR_LENGTH = 255
 
@@ -10,7 +12,7 @@ private const val VERSION_VARCHAR_LENGTH = 255
 internal object MigrationsTable : Table("migrations") {
     val version = varchar("version", VERSION_VARCHAR_LENGTH)
     val description = text("description")
-    val appliedAt = long("applied_at")
+    val appliedAt = datetime("applied_at").defaultExpression(CurrentDateTime)
 
     override val primaryKey = PrimaryKey(version)
 }
