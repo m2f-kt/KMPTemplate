266a6166211b2722557fac28a5e3e7eb58dd510b
Marc Moreno Ferrer <ignis.fatue@gmail.com>
2026-02-11 11:05:45 +0100

    feat: add initial project configuration and documentation files

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -65,7 +65,7 @@ kotlin {
             implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+            implementation(libs.compose.ui.tooling.preview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kermit)
--- composeApp/src/commonMain/kotlin/com/m2f/template/App.kt
+++ composeApp/src/commonMain/kotlin/com/m2f/template/App.kt
@@ -10,14 +10,17 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.m2f.template.di.allAppModules
 import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
-
 import template.composeapp.generated.resources.Res
 import template.composeapp.generated.resources.compose_multiplatform
 
